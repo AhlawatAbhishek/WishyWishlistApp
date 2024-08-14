df144e6ebbfc7b22967ce3b09542dd5e76291d89
AhlawatAbhishek <abhishekjobsid@pm.me>
2024-08-14 21:11:39 +0530

    Was working on displaying WishItems in LazyColumn in HomeView

--- app/src/main/java/com/mayurappstudios/wishywishlistapp/HomeView.kt
+++ app/src/main/java/com/mayurappstudios/wishywishlistapp/HomeView.kt
@@ -1,18 +1,26 @@
 package com.mayurappstudios.wishywishlistapp
 
 import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.mayurappstudios.wishywishlistapp.model.data.DummyWish
 import com.mayurappstudios.wishywishlistapp.model.data.Wish
 
 @Composable
@@ -33,12 +41,26 @@ fun HomeView(modifier: Modifier = Modifier) {
                }
         }) { innerPadding ->
         LazyColumn(modifier = Modifier.padding(innerPadding)) {
-
+               items(DummyWish.wishList) {
+                   wish-> WishItem(wish = wish) {
+                   
+               }}
         }
     }
 }
 
 @Composable
-fun WishItem(modifier: Modifier = Modifier, wish: Wish, onClick: () -> Unit){
-
+fun WishItem(modifier: Modifier = Modifier, wish: Wish, onClick: () -> Unit = {}){
+    Card(modifier = modifier
+        .fillMaxWidth()
+        .padding(top = 8.dp, start = 8.dp, end = 8.dp)
+        .clickable {
+            onClick()
+        },  elevation = 10.dp,
+        backgroundColor = Color.White) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = wish.title, color = Color.Black, fontWeight = FontWeight.ExtraBold)
+            Text(text = wish.description)
+        }
+    }
 }
--- app/src/main/java/com/mayurappstudios/wishywishlistapp/model/data/Wish.kt
+++ app/src/main/java/com/mayurappstudios/wishywishlistapp/model/data/Wish.kt
@@ -6,3 +6,32 @@ data class Wish(
     val description: String = "",
     val isWishListed: Boolean = false
 )
+
+object DummyWish {
+    val wishList = listOf(
+        Wish(
+            id = 1L,
+            title = "OnePlus 9 Pro",
+            description = "OnePlus 9 Pro is a flagship smartphone from OnePlus.",
+            isWishListed = false
+        ),
+        Wish(
+            id = 2L,
+            title = "MacBook Pro",
+            description = "MacBook Pro is a laptop from Apple.",
+            isWishListed = false
+        ),
+        Wish(
+            id = 3L,
+            title = "Samsung Galaxy S21 Ultra",
+            description = "Samsung Galaxy S21 Ultra is a flagship smartphone from Samsung.",
+            isWishListed = false
+        ),
+        Wish(
+            id = 4L,
+            title = "Google Pixel 5",
+            description = "Google Pixel 5 is a flagship smartphone from Google.",
+            isWishListed = false
+        ),
+    )
+}
