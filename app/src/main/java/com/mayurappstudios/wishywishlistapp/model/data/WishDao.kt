9a4e13cb467f36f2c116661e3de8250ca6af0044
AhlawatAbhishek <abhishekjobsid@pm.me>
2024-08-18 19:19:18 +0530

    Was working on to configure WishDatabase

--- app/src/main/java/com/mayurappstudios/wishywishlistapp/model/data/WishDao.kt
+++ app/src/main/java/com/mayurappstudios/wishywishlistapp/model/data/WishDao.kt
@@ -1,16 +1,29 @@
 package com.mayurappstudios.wishywishlistapp.model.data
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 abstract class WishDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    abstract fun addWish(wishEntity: Wish)
+    abstract suspend fun addWish(wishEntity: Wish)
 
+    //Load all the wishes from wish_table
     @Query("Select * from `wish_table`")
     abstract fun getAllWishes(): Flow<List<Wish>>
+
+    @Update
+    abstract suspend fun updateWish(wishEntity: Wish)
+
+    @Delete
+    abstract suspend fun deleteWish(wishEntity: Wish)
+
+    @Query("Select * from `wish_table` where id = :id")
+    abstract fun getWishById(id: Int): Flow<Wish>
+
 }
